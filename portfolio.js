01f1d7b41555f536e4e157383d12b711c8b0fc84
Akshay296 <akshaypandit.ece2018@gmail.com>
2018-03-09 08:06:43 +0530

    Project Completed

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){	
+	$("#image-logo").addClass("img-responsive img-circle");
+	$("#dp").addClass("img-responsive img-circle");
+	$("#portfolio1").addClass("img-responsive");
+	$("#portfolio2").addClass("img-responsive");
+	$("#portfolio3").addClass("img-responsive");
+	$("#portfolio4").addClass("img-responsive");
+	$("#portfolio5").addClass("img-responsive");
+	$("#portfolio6").addClass("img-responsive");
+
+
+
+});
